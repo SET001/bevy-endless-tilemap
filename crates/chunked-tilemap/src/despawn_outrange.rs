0aa88060ca2188fb797934365615f7df4b07ccd6
SET001 <settydark@gmail.com>
2022-11-14 23:21:33 +0200

    fixed those nasty bug when chunk despawned before it has tiles pushed as children

--- crates/chunked-tilemap/src/despawn_outrange.rs
+++ crates/chunked-tilemap/src/despawn_outrange.rs
@@ -4,16 +4,15 @@ use crate::{TilemapChunk, bundle::ChunkedTilemap};
 
 pub fn despawn_outrange_chunks(
   mut commands: Commands,
-  q_chunks: Query<(&Transform, Entity, &TilemapChunk)>,
+  q_chunks: Query<(&Transform, Entity, &TilemapChunk), With<Children>>,
   mut q_tilemaps: Query<(&mut ChunkedTilemap, &Children)>
 ){
   for (mut tilemap, children) in q_tilemaps.iter_mut(){
     for &children in children.iter(){
       if let Ok((_, entity, chunk)) =  q_chunks.get(children){    
         let range = (chunk.0 - tilemap.current_chunk).abs();
-        // if (chunk_index.x-tilemap.current_chunk.x).abs() > tilemap.range|| (chunk_index.y-tilemap.current_chunk.y).abs() > tilemap.range {
         if range.x > tilemap.range || range.y > tilemap.range {
-          debug!("despawning chunk at {:?}", chunk.0);
+          debug!("despawning chunk at {:?}-{:?}", chunk.0, entity);
           tilemap.chunks.remove(&chunk.0);
           commands.entity(entity).despawn_recursive();
         }
